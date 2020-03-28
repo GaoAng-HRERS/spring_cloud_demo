294ec7f3505b773575223b6d52b20a2e49de8e12
ang_glb <ang_glb@huaqian.com>
2020-03-28 17:37:42 +0800

    测试git冲突

--- seata-order-service2001/src/main/java/com/atguigu/springcloud/alibaba/config/MyBatisConfig.java
+++ seata-order-service2001/src/main/java/com/atguigu/springcloud/alibaba/config/MyBatisConfig.java
@@ -30,8 +30,14 @@ public class MyBatisConfig {
 
     @Value("${mybatis.mapper-locations}")
     private String mapperLocations;
-    
-    
+
+    /**
+     *测试git冲突
+     */
+    boolean a= false;
+
+    int i = 0;
+
     /**
      * @param sqlSessionFactory SqlSessionFactory
      * @return SqlSessionTemplate
